052d484fcce51a26741a467749906f0e81ea6637
Christian Hillebrand <christianhillebrand@Fakultat73siMac.fritz.box>
2021-03-11 15:19:23 +0100

    final carousel

--- carousel/src/Carsl.js
+++ carousel/src/Carsl.js
@@ -1,5 +1,7 @@
 import React,{Component} from 'react';
 import './App.css';
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
  
 
 let styles = {
@@ -7,25 +9,22 @@ let styles = {
     width: '500px'
   };
 
-  /* Use this data to create an array of objects that you loop through
-  to create the carousel. 
+  const IMAGE_ARRAY = [
+    {city: "Hong Kong", picture: "https://res.klook.com/image/upload/fl_lossy.progressive,q_65/c_fill,w_480,h_384/cities/jrfyzvgzvhs1iylduuhj.jpg" }, 
+    {city: "Singapore", picture: "https://res.klook.com/image/upload/fl_lossy.progressive,q_65/c_fill,w_480,h_384/cities/c1cklkyp6ms02tougufx.webp"}, 
+    {city: "Japan", picture: "https://res.klook.com/image/upload/fl_lossy.progressive,q_65/c_fill,w_480,h_384/cities/e8fnw35p6zgusq218foj.webp"}, 
+    {city: "Las Vegas", picture: "https://res.klook.com/image/upload/fl_lossy.progressive,q_65/c_fill,w_480,h_384/cities/liw377az16sxmp9a6ylg.webp"}
+  ];
 
-  Hong Kong: https://res.klook.com/image/upload/fl_lossy.progressive,q_65/c_fill,w_480,h_384/cities/jrfyzvgzvhs1iylduuhj.jpg
-  Singapore: https://res.klook.com/image/upload/fl_lossy.progressive,q_65/c_fill,w_480,h_384/cities/c1cklkyp6ms02tougufx.webp
-  Japan: https://res.klook.com/image/upload/fl_lossy.progressive,q_65/c_fill,w_480,h_384/cities/e8fnw35p6zgusq218foj.webp
-  Las Vegas: https://res.klook.com/image/upload/fl_lossy.progressive,q_65/c_fill,w_480,h_384/cities/liw377az16sxmp9a6ylg.webp
-
-  Use the react-responsive-carousel package to create the Carousel: 
-  https://www.npmjs.com/package/react-responsive-carousel
-
-
-  */
 
 const Carsl = () => {
 	return (
 		<div style={styles}>
-			 Welcome to the Carousel..
+			 <Carousel>
+               {IMAGE_ARRAY.map((element) =>  <img src={element.picture} alt={element.city}/>)}
+        </Carousel>
 		</div>
 	  )
 }
+
 export default Carsl;
